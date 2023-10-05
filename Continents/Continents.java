ef7af5dc9f1b30acdb188e409312a8e5bd20b6c0
llucasBorba <lucasborba6075@gmail.com>
2023-10-04 22:51:48 -0300

    All the code from Continents

--- Continents/Continents.java
+++ Continents/Continents.java
@@ -6,32 +6,16 @@ public class Continents{
 
     public void largestCity(int continents){
         this.continents = continents;
-        switch(continents){
-            case 1:
-                System.out.println("North America: Mexico City, Mexico.");
-                break;
-            case 2:
-                System.out.println("South America: Sao Paulo, Brazil.");
-                break;
-            case 3:
-                System.out.println("Europe: Moscow, Russia");
-                break;
-            case 4:
-                System.out.println("Africa: Lagos, Nigeria.");
-                break;
-            case 5:
-                System.out.println("Asia: Shanghai, China.");
-                break;
-            case 6:
-                System.out.println("Australia: Sydney, Australia.");
-                break;
-            case 7:
-                System.out.println("Antarctica: McMurdo Station, US");
-                break;
-            default:
-                System.out.println("Undefined continent!");
-
-        };
+        switch (continents) {
+            case 1 -> System.out.println("North America: Mexico City, Mexico.");
+            case 2 -> System.out.println("South America: Sao Paulo, Brazil.");
+            case 3 -> System.out.println("Europe: Moscow, Russia");
+            case 4 -> System.out.println("Africa: Lagos, Nigeria.");
+            case 5 -> System.out.println("Asia: Shanghai, China.");
+            case 6 -> System.out.println("Australia: Sydney, Australia.");
+            case 7 -> System.out.println("Antarctica: McMurdo Station, US");
+            default -> System.out.println("Undefined continent!");
+        }
     }
 
     public static void main (String[] args){
