394db71b6f5d4c04ea5a40503004caa9dbbcad06
llucasBorba <lucasborba6075@gmail.com>
2023-10-18 18:29:49 -0300

    FizzBuzz

--- PrimeDirective/PrimeDirective.java
+++ PrimeDirective/PrimeDirective.java
@@ -3,20 +3,35 @@ import java.util.ArrayList;
 
 public class PrimeDirective {
 
-    public  boolean isPrime(ArrayList<Integer> numbers){
-        for (int i = 0; i < numbers.size(); i++ ) {
-            if ( numbers.get(i) > 1 && numbers.get(i) / i == 1){
-                return true;
+    public  boolean isPrime(int numbe){
+            if ( numbe <= 1){
+                return false;
             }
+            for (int i = 2; i * i <= numbe; i++) {
+                if( numbe % i == 0 ) {
+                    return false;
+                }
+            }
+            return true;
+    }
+
+
+    public static void main(String[] args){
+
+            PrimeDirective pd = new PrimeDirective();
+
+            int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
+            ArrayList<Integer> numero = new ArrayList<>();
+            for (int number : numbers) {
+                numero.add(number);
+            }
+
+            for (int numeros: numero){
+                if(pd.isPrime(numeros)){
+                    System.out.println( numero + "é primo");
+                } else
+                    System.out.println(numero +  "nào é primo");
+            }
+
         }
-        return false;
-    }
-    public static void main(String[] args) {
-
-        PrimeDirective pd = new PrimeDirective();
-        int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
-
-        System.out.println(isPrime(numbers));
-
-    }
 }
