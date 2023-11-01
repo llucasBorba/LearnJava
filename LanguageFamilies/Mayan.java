d0d939bba235b5afffeada822f2c75a957f5760d
llucasBorba <lucasborba6075@gmail.com>
2023-11-01 11:31:17 -0300

    Maya class

--- LanguageFamilies/Mayan.java
+++ LanguageFamilies/Mayan.java
@@ -1,4 +1,16 @@
 package LanguageFamilies;
 
-public class Mayan {
+public class Mayan extends Language{
+
+    public Mayan(String name, int numSpeakers){
+        super(name, numSpeakers, "Central America", "verb-object-subject");
+    }
+
+    @Override
+    public void getInfo(){
+        System.out.println("\n" + name + " is spoken by " + numSpeakers + " people mainly in " + regionsSpoken + ".");
+        System.out.println("The language follows the word order: " + wordOrder+ ".");
+        System.out.println("Fun fact: " + name + " is an ergative language.");
+    }
+
 }
