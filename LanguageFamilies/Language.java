3e6c38e75d6fa62505d9b8a554513a59f42ec233
llucasBorba <lucasborba6075@gmail.com>
2023-11-01 11:19:26 -0300

    Language Class

--- LanguageFamilies/Language.java
+++ LanguageFamilies/Language.java
@@ -1,4 +1,30 @@
 package LanguageFamilies;
 
-public class Language {
+public class Language{
+
+protected String name;
+protected int numSpeakers;
+protected String regionsSpoken;
+protected String wordOrder;
+
+public Language(String name, int numSpeakers, String regionsSpoken, String wordOrder){
+
+        this.name = name;
+        this.numSpeakers = numSpeakers;
+        this.regionsSpoken = regionsSpoken;
+        this.wordOrder = wordOrder;
+        }
+
+public void getInfo(){
+        System.out.println(name + " is spoken by " + numSpeakers + " people mainly in " + regionsSpoken + ".");
+        System.out.println("The language follows the word order:" + wordOrder+ ".");
+
+
+        }
+
+public static void main(String[] args){
+        Language spanish = new Language("Spanish", 100000, "America", "Verb-Object");
+        spanish.getInfo();
+
+        }
 }
