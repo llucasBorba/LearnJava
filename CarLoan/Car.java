2d2fc07990234524beda884b5eb45e76552ec036
llucasBorba <lucasborba6075@gmail.com>
2023-11-09 22:54:29 -0300

    Searilation teste

---
+++ CarLoan/Car.java
@@ -0,0 +1,27 @@
+package CarLoan;
+
+import java.io.Serializable;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Car implements Serializable {
+    private String make;
+    private int year;
+    private static final long serialVersionUID = 1L;
+
+    public Car(String make, int year) {
+        this.make = make;
+        this.year = year;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        Car toyota = new Car("Toyota", 2021);
+        Car honda = new Car("Honda", 2020);
+        FileOutputStream fileOutputStream = new FileOutputStream("cars.txt");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(toyota);
+        objectOutputStream.writeObject(honda);
+    }
+}
