34a3b12b1496c0f50db7418d07b926e7d4326386
llucasBorba <lucasborba6075@gmail.com>
2023-11-13 10:32:00 -0300

    Testing Serializable

--- CarLoan/Car.java
+++ CarLoan/Car.java
@@ -1,11 +1,15 @@
 package CarLoan;
-
-import java.io.*;
+import java.io.Serializable;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Car implements Serializable {
     private String make;
     private int year;
-    @Serial
     private static final long serialVersionUID = 1L;
 
     public Car(String make, int year) {
@@ -13,12 +17,27 @@ public class Car implements Serializable {
         this.year = year;
     }
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public String toString(){
+        return String.format("Car make is: %s, Car year is: %d", this.make, this.year);
+    }
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Car toyota = new Car("Toyota", 2021);
         Car honda = new Car("Honda", 2020);
         FileOutputStream fileOutputStream = new FileOutputStream("cars.txt");
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
         objectOutputStream.writeObject(toyota);
         objectOutputStream.writeObject(honda);
+
+        FileInputStream fileInputStream = new FileInputStream("cars.txt");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+        Car toyotaCopy = (Car) objectInputStream.readObject();
+        Car hondaCopy = (Car) objectInputStream.readObject();
+
+        boolean isSameObject = toyotaCopy == toyota;
+        System.out.println("Toyota (Copy) - "+toyotaCopy);
+        System.out.println("Toyota (Original) - "+toyota);
+        System.out.println("Is same object: "+ isSameObject);
     }
 }
