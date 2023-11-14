55d23fe1f6479e0394a1e72cefeadac0eeac4e0c
llucasBorba <lucasborba6075@gmail.com>
2023-11-14 15:57:24 -0300

    Testing Serializable

--- CarLoan/Car.java
+++ CarLoan/Car.java
@@ -7,21 +7,48 @@ import java.io.ObjectInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import java.io.Serializable;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Car implements Serializable {
     private String make;
     private int year;
     private static final long serialVersionUID = 1L;
+    private Engine engine;
 
     public Car(String make, int year) {
         this.make = make;
         this.year = year;
+        this.engine = new Engine(2.4, 6);
     }
+
+    private void writeObject(ObjectOutputStream stream) throws IOException {
+        stream.writeObject(this.make);
+        stream.writeInt(this.year);
+        stream.writeDouble(this.engine.getLiters());
+        stream.writeInt(this.engine.getCylinders());
+    }
+
+    private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
+
+        this.make = (String) stream.readObject();
+        this.year = (int) stream.readInt();
+        double liters = (double) stream.readDouble();
+        int cylinders = (int) stream.readInt();
+        this.engine = new Engine(liters, cylinders);
+    }
+
 
     public String toString(){
-        return String.format("Car make is: %s, Car year is: %d", this.make, this.year);
+        return String.format("Car make is: %s, Car year is: %d, %s", this.make, this.year, this.engine);
     }
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
         Car toyota = new Car("Toyota", 2021);
         Car honda = new Car("Honda", 2020);
         FileOutputStream fileOutputStream = new FileOutputStream("cars.txt");
---
+++ CarLoan/Engine.java
@@ -0,0 +1,24 @@
+package CarLoan;
+
+    public class Engine {
+        private double liters;
+        private int cylinders;
+
+        public Engine(double liters, int cylinders) {
+            this.liters = liters;
+            this.cylinders = cylinders;
+        }
+
+        public double getLiters() {
+            return this.liters;
+        }
+
+        public int getCylinders() {
+            return this.cylinders;
+        }
+
+        public String toString() {
+            return String.format("Engine liters is: %f, Engine cylinders is: %d", this.liters, this.cylinders);
+        }
+    }
+
